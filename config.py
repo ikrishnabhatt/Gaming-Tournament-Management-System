1747ef0d77876935c1a5c41905df5c8a2eb94ed7
ikrishnabhatt <krishnabhatt2230@gmail.com>
2024-04-28 15:09:31 +0530

    used sql lite

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    # Database configuration
-    DB_FILE = "tournament.db"
-
-    # Secret key for session management (replace with a random string)
-    SECRET_KEY = "iamasecretkey"
